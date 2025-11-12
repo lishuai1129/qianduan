c780a8886c7ab321ac45d0c45d630128b5a04c1e
lishuai1129 <2728701176@qq.com>
2025-11-12 14:42:32 +0800

    src结束

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-    <Count/>
-    <br>
-    <LoveTalk/>  
+  <div class="app">
+    <h2>我是App组件</h2>
+    <Suspense>
+      <template v-slot:default>
+        <Child/>
+      </template>
+      <!-- <template v-slot:fallback> -->
+        <!-- <h2>加载中......</h2> -->
+      <!-- </template> -->
+    </Suspense>
+  </div>
 </template>
 
-<script lang="ts" setup name="App">
-import Count from './components/Count.vue';
-import LoveTalk from './components/LoveTalk.vue';
+<script setup lang="ts" name="App">
+  import {Suspense} from 'vue'
+  import Child from './Child.vue'
 </script>
 
-<style scoped>
+<style>
+  .app {
+    background-color: #ddd;
+    border-radius: 10px;
+    padding: 10px;
+    box-shadow: 0 0 10px;
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
-// 引入createApp用于创建应用
-import { createApp } from 'vue'
-// 引入App根组件
+import {createApp} from 'vue'
 import App from './App.vue'
-// 引入pinia
-import { createPinia } from 'pinia'
 
+// 创建应用
 const app = createApp(App)
-// 创建pinia
-const pinia = createPinia()
-// 安装pinia
-app.use(pinia)
+
 // 挂载应用
 app.mount('#app')
-
-
-// createApp(App).mount('#app')
